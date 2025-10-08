ef578edae4450cc967da836903a5e3accedbc41b
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2025-10-08 17:37:20 +0200

    10-08-2025

---
+++ update_cachi.cmd
@@ -0,0 +1,8 @@
+cd /raid/nwarg/vacon.github.io
+mkdir ../vacon_foo
+mv ClimData/*.png ../vacon_foo
+git pull
+mv ../vacon_foo/*.png ClimData
+rm -fr ../vacon_foo
+#bundle exec jekyll build
+#JEKYLL_ENV=production bundle exec jekyll build
