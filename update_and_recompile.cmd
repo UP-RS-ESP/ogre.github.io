f6811c2df76cb3314287aa853e7206c91cab10c3
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2025-10-08 08:36:11 +0200

    10-08-2025

--- update_and_recompile.cmd
+++ update_and_recompile.cmd
@@ -1,4 +1,8 @@
 cd /home/bodo/vacon.github.io
+mkdir ../vacon_foo
+mv ClimData/*.png ../vacon_foo
 git pull
+mv ../vacon_foo/*.png ClimData
+rm -fr ../vacon_foo
 #bundle exec jekyll build
 JEKYLL_ENV=production bundle exec jekyll build
