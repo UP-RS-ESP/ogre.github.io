6e5b3e56598014a1a9fb7b1619f024b36bac6fce
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2026-01-02 14:06:08 +0100

    01-02-2026

--- ClimData/codes/mk_StromPi_plots_cachi.cmd
+++ ClimData/codes/mk_StromPi_plots_cachi.cmd
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+. /home/bodo/miniconda3/etc/profile.d/conda.sh
+conda activate py310
+
 cd /raid/nwarg/vacon.github.io/ClimData/
 python3 /raid/nwarg/vacon.github.io/ClimData/codes/read_StromPi_Logs.py /raid/nwarg/vacon/prt_data
 
