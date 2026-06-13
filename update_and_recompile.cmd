55e9150d93493ccdec547f9d38af97001d516660
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2026-06-13 09:37:41 +0200

    06-13-2026

--- update_and_recompile.cmd
+++ update_and_recompile.cmd
@@ -2,8 +2,12 @@ cd /home/bodo/tron.github.io
 #cd /raid/nwarg/tron.github.io
 mkdir ../tron_foo
 mv ClimData/*.png ../tron_foo
+mv ClimData/*.jpg ../tron_foo
+mv ClimData/*.bz2 ../tron_foo
 git pull
 mv ../tron_foo/*.png ClimData
+mv ../tron_foo/*.jpg ClimData
+mv ../tron_foo/*.bz2 ClimData
 rm -fr ../tron_foo
 #bundle exec jekyll build
 JEKYLL_ENV=production bundle exec jekyll build
--- update_cachi.cmd
+++ update_cachi.cmd
@@ -1,8 +1,12 @@
 cd /raid/Nepal/tron.github.io
 mkdir ../tron_foo
 mv ClimData/*.png ../tron_foo
+mv ClimData/*.jpg ../tron_foo
+mv ClimData/*.bz2 ../tron_foo
 git pull
 mv ../tron_foo/*.png ClimData
+mv ../tron_foo/*.jpg ClimData
+mv ../tron_foo/*.bz2 ClimData
 rm -fr ../tron_foo
 #bundle exec jekyll build
 #JEKYLL_ENV=production bundle exec jekyll build
