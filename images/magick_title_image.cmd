054b3567aa787efdd62b6d0382a632afb1f5c740
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2026-06-13 13:45:55 +0200

    06-13-2026

--- images/magick_title_image.cmd
+++ images/magick_title_image.cmd
@@ -1,1 +1,1 @@
-magick Trishuli_hillslope_photo_portrait.jpg -resize 30% -bordercolor transparent  -border 0x100  Trishuli_hillslope_photo_portrait_lr.jpg
+magick Trishuli_hillslope_photo_portrait.jpg -resize 37% -bordercolor '#eeeeee'  -border 0x100  Trishuli_hillslope_photo_portrait_lr.jpg
